be929498395be43e741d885ec0970b6c9fb6644e
egekaradibak <ege.karadibak@mail.mcgill.ca>
2022-12-04 15:44:11 -0500

    Added volunteer status to the Parent class

--- src/main/java/com/school/schedule/app/Parent.java
+++ src/main/java/com/school/schedule/app/Parent.java
@@ -3,11 +3,13 @@ package com.school.schedule.app;
 public class Parent extends Person{
 
     private float phoneNumber;
+    private boolean isVolunteer;
 
     // constructor
-    public Parent(float id, String firstName, String lastName, float phoneNumber) {
+    public Parent(float id, String firstName, String lastName, float phoneNumber, boolean isVolunteer) {
         super(id, firstName, lastName);
         this.phoneNumber = phoneNumber;
+        this.isVolunteer = isVolunteer;
     }
 
     // Getter Methods
@@ -16,9 +18,17 @@ public class Parent extends Person{
         return phoneNumber;
     }
 
+    public boolean getIsVolunteer() {
+        return isVolunteer;
+    }
+
     // Setter Methods
 
     public void setPhoneNumber( float phoneNumber ) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public void setIsVolunteer( boolean phoneNumber ) {
+        this.isVolunteer = isVolunteer;
     }
 }
