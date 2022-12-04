17ed7ef98fb5feb1724c2a235fcbe63994b3e2a6
marta-matu <marta.matuszewski@mail.mcgill.ca>
2022-12-04 14:29:12 -0400

    Undo change

--- src/main/java/com/school/schedule/app/Teacher.java
+++ src/main/java/com/school/schedule/app/Teacher.java
@@ -2,24 +2,15 @@ package com.school.schedule.app;
 
 public class Teacher extends Person{
 
-    private Boolean hasTenure;
     private Schedule schedule;
 
     // Getter Methods
-
-    public Boolean getHasTenure() {
-        return hasTenure;
-    }
 
     public Schedule getSchedule() {
         return schedule;
     }
 
     // Setter Methods
-
-    public void setHasTenure( Boolean hasTenure ) {
-        this.hasTenure = hasTenure;
-    }
 
     public void setSchedule( Schedule schedule ) {
         this.schedule = schedule;
