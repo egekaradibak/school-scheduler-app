f3d06bac6b0508b76bb692830e0ee5a0890f60a9
marta-matu <marta.matuszewski@mail.mcgill.ca>
2022-12-05 16:00:04 -0500

    add teacher menu options

--- src/main/java/com/school/schedule/app/App.java
+++ src/main/java/com/school/schedule/app/App.java
@@ -6,7 +6,6 @@ public class App
 {
     private static boolean studentMenu(School school, Scanner sc){
         System.out.println( "\nSTUDENT MENU" );
-        System.out.println( "\nPlease select an option by entering the number." );
         System.out.println( "1. Select an existing student" );
         System.out.println( "2. Add a new student" );
         System.out.print( "Selection: " );
@@ -15,7 +14,7 @@ public class App
         Student student;
 
         if (optionSelected.equals("1")){
-            // Display list of students
+            // Display selected student info
             System.out.println( "\nPlease enter the student's ID." );
             System.out.print( "ID: " );
             int id = sc.nextInt();
@@ -56,9 +55,9 @@ public class App
         System.out.println( "\n-----------------------------\nStudent Information:\n" + student + "\n-----------------------------");
 
         System.out.println( "\nSELECTED STUDENT OPTIONS" );
-        System.out.println( "\nPlease select an option by entering the number." );
         System.out.println( "1. Add a course to the student's schedule." );
         System.out.println( "2. Add a Parent to the student." );
+        System.out.println( "3. Do nothing." );
         System.out.print( "Selection: " );
         optionSelected = sc.nextLine();
 
@@ -72,6 +71,8 @@ public class App
         } else if(optionSelected.equals("2")){
             // Add a parent
             addParentToStudent(student, sc);
+        } else if (optionSelected.equals("3")){
+            return false;
         } else if(optionSelected.equals("q")){
             return true;
         } else{
@@ -83,9 +84,74 @@ public class App
         return false;
     }
 
+    private static boolean teacherMenu(School school, Scanner sc){
+        System.out.println( "\nTEACHER MENU" );
+        System.out.println( "1. Select an existing teacher" );
+        System.out.println( "2. Add a new teacher" );
+        System.out.print( "Selection: " );
+        String optionSelected = sc.nextLine();
+
+        Teacher teacher;
+
+        if (optionSelected.equals("1")){
+            // Select a teacher
+            teacher = getTeacher(school, sc);
+            if (teacher == null){
+                return false;
+            }
+        } else if(optionSelected.equals("2")){
+            //Create new teacher
+            System.out.println( "\nPlease follow the prompts to enter information about the teacher." );
+            System.out.print( "ID: " );
+            int id = sc.nextInt();
+            sc.nextLine();
+            System.out.print( "First name: " );
+            String firstName = sc.nextLine();
+            System.out.print( "Last name: " );
+            String lastname = sc.nextLine();
+
+            Teacher existingTeacher = school.getTeacherById(id);
+            if (existingTeacher != null){
+                System.out.println( "\nERROR: A teacher with id " + id + " already exists.\n");
+                return false;
+            }
+
+            teacher = new Teacher(id, firstName, lastname);
+            school.addTeacher(teacher);
+        } else if(optionSelected.equals("q")){
+            return true;
+        } else{
+            System.out.println( "Invalid option selected." );
+            return false;
+        }
+
+        System.out.println( "\n-----------------------------\nTeacher Information:\n" + teacher + "\n-----------------------------");
+
+        System.out.println( "\nSELECTED TEACHER OPTIONS\n" );
+        System.out.println( "1. Add a course to the teacher's schedule." );
+        System.out.println( "2. Do nothing." );
+        System.out.print( "Selection: " );
+        optionSelected = sc.nextLine();
+
+        if (optionSelected.equals("1")){
+            // Add a course
+            Course course = getCourse(school,sc);
+            if (course != null){
+                teacher.getSchedule().addCourse(course);
+            }
+        } else if (optionSelected.equals("2")){
+            return false;
+        } else{
+            System.out.println( "Invalid option selected." );
+            return false;
+        }
+
+        System.out.println( "\n-----------------------------\nUpdated Teacher Information:\n" + teacher + "\n-----------------------------");
+        return false;
+    }
+
     private static boolean courseMenu(School school, Scanner sc){
         System.out.println( "\nCOURSE MENU" );
-        System.out.println( "\nPlease select an option by entering the number." );
         System.out.println( "1. Select an existing course" );
         System.out.println( "2. Add a new course" );
         System.out.print( "Selection: " );
@@ -107,19 +173,15 @@ public class App
             int id = sc.nextInt();
             sc.nextLine();
             System.out.print( "classroom: " );
-            String classroom = sc.nextLine(); 
-            System.out.print( "teacher id: " );
-            int teacherId = sc.nextInt();
-            sc.nextLine();
-
-            //TODO: get teacher
+            String classroom = sc.nextLine();
+            Teacher teacher = getTeacher(school, sc);
 
             Course existingCourse = school.getCourseById(id);
             if (existingCourse != null){
                 System.out.println( "\nERROR: A course with id " + id + " already exists.\n");
                 return false;
             }
-            course = new Course(name, teacherId, classroom, null);
+            course = new Course(name, id, classroom, teacher);
             school.addCourse(course);
         } else if(optionSelected.equals("q")){
             return true;
@@ -162,9 +224,22 @@ public class App
         }
         return course;
     }
+
+    private static Teacher getTeacher(School school, Scanner sc){
+        System.out.print( "Teacher ID: " );
+        int id = sc.nextInt();
+        sc.nextLine();
+        Teacher teacher = school.getTeacherById(id);
+        if (teacher == null){
+            System.out.println( "\nERROR: A teacher with id " + id + " does not exist.\n");
+        }
+        return teacher;
+    }
     public static void main( String[] args )
     {   
         System.out.println( "Welcome to the School Scheduler App! " );
+        System.out.println( "- Select menu options by entering their corresponding number." );
+        System.out.println( "- Quit the application by entering 'q'");
         Scanner sc = new Scanner(System.in);
 
         System.out.println( "\nPlease follow the prompts to enter information about your school." );
@@ -180,8 +255,8 @@ public class App
 
         boolean quit = false;
         while (!quit){
-            System.out.println( "\n-----------------------------\nSchool Information:\n" + school + "\n-----------------------------");
-            System.out.println( "\nPlease select a menu option by entering the number." );
+            System.out.println( "\n-----------------------------\nSchool Information:\n" + school + "\n-----------------------------\n");
+            System.out.println( "MAIN MENU" );
             System.out.println( "1. Student Menu" );
             System.out.println( "2. Teacher Menu" );
             System.out.println( "3. Courses Menu" );
@@ -194,6 +269,7 @@ public class App
 
             } else if(optionSelected.equals("2")){
                 // Display teacher menu options
+                quit = teacherMenu(school, sc);
 
             } else if(optionSelected.equals("3")){
                 // Display courses menu options
